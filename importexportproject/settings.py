dc6b60c51ff8aba0a13153819187a12a1f6eef6c
kevinnsrt <siraitkevin25@gmail.com>
2026-06-26 20:28:25 +0700

    adding requirements for cloud

--- importexportproject/settings.py
+++ importexportproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9(4+#%^sa3-es*mtg5iscnz^11!2%n0#^v(m&3n(((1a5#u6#&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
