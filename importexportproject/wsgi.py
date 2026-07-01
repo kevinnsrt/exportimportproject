7b0a23779ccfe08a51d8b5ca6ca30c663f892231
kevinnsrt <siraitkevin25@gmail.com>
2026-07-01 14:05:48 +0700

    fix: pindahkan auto-migrate ke wsgi.py

--- importexportproject/wsgi.py
+++ importexportproject/wsgi.py
@@ -1,17 +1,19 @@
-"""
-WSGI config for importexportproject project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
-
 import os
-
+import django
 from django.core.wsgi import get_wsgi_application
+from django.core.management import call_command
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'importexportproject.settings')
 
+# Inisialisasi Django terlebih dahulu
+django.setup()
+
+# OTOMATIS JALANKAN MIGRASI SAAT VERCEL NYALA
+try:
+    print("Menjalankan migrasi database ke Neon Postgres...")
+    call_command('migrate', interactive=False)
+    print("Migrasi sukses!")
+except Exception as e:
+    print(f"Gagal menjalankan migrasi otomatis: {e}")
+
 application = get_wsgi_application()
-app = application
