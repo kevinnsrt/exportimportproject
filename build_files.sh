a5293ffb94e65f4f633115604f2c746c315563f3
kevinnsrt <siraitkevin25@gmail.com>
2026-07-01 14:03:06 +0700

    fix: daftarkan static-build dan jalankan migrasi

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-# 1. Install semua requirements
+# 1. Install dependensi
 python3 -m pip install -r requirements.txt
 
 # 2. Jalankan migrasi database ke Postgres Neon
 python3 manage.py migrate --noinput
+
+# 3. Kumpulkan file statis (opsional tapi disarankan untuk Vercel)
+python3 manage.py collectstatic --noinput --clear
