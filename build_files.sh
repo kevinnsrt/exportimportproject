53b0d3dadf51634f769a3a784f1b9966ba16cef5
kevinnsrt <siraitkevin25@gmail.com>
2026-07-01 14:04:10 +0700

    fix: bersihkan format LF dan beri izin eksekusi build_files

--- build_files.sh
+++ build_files.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-
-# 1. Install dependensi
 python3 -m pip install -r requirements.txt
-
-# 2. Jalankan migrasi database ke Postgres Neon
 python3 manage.py migrate --noinput
-
-# 3. Kumpulkan file statis (opsional tapi disarankan untuk Vercel)
-python3 manage.py collectstatic --noinput --clear
